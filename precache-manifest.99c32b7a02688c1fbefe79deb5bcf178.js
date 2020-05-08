fd671d0cdc74c28a2bdf6cd95c4fa2dd4d6b90e3
unknown <precie2010.pd@gmail.com>
2020-05-08 16:20:36 -0600

    Updates

---
+++ precache-manifest.99c32b7a02688c1fbefe79deb5bcf178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54968b8ae29a4aaf5c9966e3026f400c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "24da3e0175e1f15819c7",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "272374de67e394b35969",
+    "url": "/robofriends/static/css/main.bf443cee.chunk.css"
+  },
+  {
+    "revision": "24da3e0175e1f15819c7",
+    "url": "/robofriends/static/js/2.3a086a5c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends/static/js/2.3a086a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272374de67e394b35969",
+    "url": "/robofriends/static/js/main.635e11a3.chunk.js"
+  },
+  {
+    "revision": "a74df91c98f4534faa83",
+    "url": "/robofriends/static/js/runtime-main.492c80e6.js"
+  }
+]);
